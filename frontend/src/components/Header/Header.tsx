e8cddb01c88e02e7b58ac6030bb30e53c82c9498
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-02-07 16:00:28 +0530

    header done

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
 import styles from "./Header.module.css";
-const Header = () => {
+type headerTypes = {
+  restaurantName: string;
+  estd: number;
+}
+const Header = ({ restaurantName, estd }: headerTypes) => {
   return (
-    <>
-      Header component
-    </>
+    <header className={styles.header}>
+      <h1> Welcome To</h1>
+      <h2>{restaurantName.toUpperCase()}</h2>
+      <p>since: {estd}</p>
+    </header>
   )
 }
 
