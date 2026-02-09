4ebad3a66a346987088096b3dfe0b07e66e9c6fd
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-02-09 22:44:11 +0530

    added categories

--- frontend/src/components/Menubar/Menubar.tsx
+++ frontend/src/components/Menubar/Menubar.tsx
@@ -1,17 +1,48 @@
-import styles from './Menubar.module.css'
+import styles from './Menubar.module.css';
+import categories from "../../assets/demo_category.json";
+import Btn from '../../UI/Btn/Btn';
+import { useState } from 'react';
 
 interface MenubarProps {
   isOpen: boolean;
 }
 
 const Menubar = ({ isOpen }: MenubarProps) => {
-  return (
-    <aside className={styles.aside} style={{
-      left: isOpen ? "0" : "-100%"
-    }}>
-      hii
-    </aside>
-  )
-}
+  const category = categories.category;
 
-export default Menubar
+  // store which button is active
+  const [activeIndex, setActiveIndex] = useState<number | null>(-1);
+
+  const handleOnClick = (index: number) => {
+    setActiveIndex(prev => (prev === index ? null : index));
+  };
+
+  return (
+    <aside
+      className={styles.aside}
+      style={{ left: isOpen ? "0" : "-100%" }}
+    >
+      <h1>Categories:</h1>
+
+      <div className={styles.categories}>
+        <Btn
+          text={"All"}
+          onClick={() => handleOnClick(-1)}
+          className={`${styles.btns} ${activeIndex === -1 ? styles.active : ""
+            }`}
+        />
+        {category.map((item, index) => (
+          <Btn
+            key={index}
+            text={item}
+            onClick={() => handleOnClick(index)}
+            className={`${styles.btns} ${activeIndex === index ? styles.active : ""
+              }`}
+          />
+        ))}
+      </div>
+    </aside>
+  );
+};
+
+export default Menubar;
